a050be41cf113f454c818e5c13eb27d2ee7fa4c1
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-06-19 03:02:01 +0200

    Feat: ProjectController function example created

---
+++ src/controllers/project.controller.ts
@@ -0,0 +1,9 @@
+// import Project from "../models/Project"
+// import type { Request, Response } from "express"
+
+// export const ProjectControl = async (req: Request, res: Response) => {
+//   res.status(201).json({
+//     success: true,
+//     message: "get all the projects, men",
+//   })
+// }

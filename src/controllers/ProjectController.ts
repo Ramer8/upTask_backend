2ca0097c29822129e792673de4ab32c746137fc6
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-06-19 03:28:44 +0200

    Feat: createProjects example & his route created and tested in thunder client

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,6 +1,10 @@
 import type { Request, Response } from "express"
 
 export class ProjectController {
+  static createProjects = async (req: Request, res: Response) => {
+    res.send("creating projects ...")
+  }
+
   static getAllProjects = async (req: Request, res: Response) => {
     res.send("All the projects from ProjectController")
   }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -4,6 +4,8 @@ import { ProjectController } from "../controllers/ProjectController"
 
 const router = Router()
 
+router.post("/", ProjectController.createProjects)
+
 router.get("/", ProjectController.getAllProjects)
 // router.get("/", ProjectControl)
 export default router
